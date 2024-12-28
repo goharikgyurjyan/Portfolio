c86ab70720789e222b48844a6d5f20d9a8188dfd
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-12-28 14:30:20 +0100

    modif accueil, ajout planetes + animation

--- js/planet.js
+++ js/planet.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const planets = document.querySelectorAll('.planet');
-    const container = document.querySelector('.position-relative');
-
+    const planets = document.querySelectorAll('.float');
     planets.forEach(planet => {
         const randomTop = Math.random() * 80 + 10;
         const randomLeft = Math.random() * 80;
-
+        planet.style.position = 'absolute';
         planet.style.top = `${randomTop}%`;
         planet.style.left = `${randomLeft}%`;
     });
+});
+
+
+document.addEventListener('mousemove', (e) => {
+    const star = document.createElement('div');
+    star.classList.add('star');
+
+    star.style.top = `${e.clientY}px`;
+    star.style.left = `${e.clientX}px`;
+
+    document.body.appendChild(star);
+
+    setTimeout(() => {
+        star.remove();
+    }, 2000);
 });
